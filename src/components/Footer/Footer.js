02eed4ff62ca29c04ad2cd5befc042ad50b0a425
Sayyed Furqan Hussain <furqansa344@gmail.com>
2024-11-26 21:51:40 +0530

    Update Footer.js

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,10 +3,10 @@ import './Footer.css'
 const Footer = () => (
   <footer className='footer'>
     <a
-      href='https://github.com/rjshkhr/cleanfolio'
+      href='https://github.com/04092000f'
       className='link footer__link'
     >
-      Created By Raj Shekhar
+      Visit my Repository
     </a>
   </footer>
 )
